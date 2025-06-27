015438c865a0923caee8137fdb88d4d2a7c8179f
sunryze-git <48110960+sunryze-git@users.noreply.github.com>
2025-06-27 00:23:52 -0400

    add better randomness

--- MusicBot/Utilities/ListExtensions.cs
+++ MusicBot/Utilities/ListExtensions.cs
@@ -1,16 +1,16 @@
+using System.Security.Cryptography;
+
 namespace MusicBot.Utilities;
 
 public static class ListExtensions
 {
-    private static readonly Random Rng = new Random();
-
     public static void Shuffle<T>(this IList<T> list)
     {
         var n = list.Count;
         while (n > 1)
         {
             n--;
-            var k = Rng.Next(n + 1);
+            var k = RandomNumberGenerator.GetInt32(n+1);;
             (list[k], list[n]) = (list[n], list[k]);
         }
     }
