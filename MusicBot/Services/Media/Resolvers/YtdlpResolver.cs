664e3cbc38919efdfbccc8fd6a141b02805a21cb
sunryze-git <48110960+sunryze-git@users.noreply.github.com>
2025-06-27 00:23:38 -0400

    add yt-dlp resolver

---
+++ MusicBot/Services/Media/Resolvers/YtdlpResolver.cs
@@ -0,0 +1,25 @@
+using MusicBot.Services.Media.Backends;
+using YoutubeExplode.Videos;
+
+namespace MusicBot.Services.Media.Resolvers;
+
+public class YtdlpResolver(DlpBackend dlpService) : IMediaResolver
+{
+    public string Name => "YT-DLP";
+    public int Priority => 3;
+    public Task<bool> CanResolveAsync(string query)
+    {
+        return Task.FromResult(Uri.IsWellFormedUriString(query, UriKind.Absolute));
+    }
+
+    public async Task<IReadOnlyList<IVideo>> ResolveAsync(string query)
+    {
+        var results = await dlpService.GetMetadataAsync(query);
+        return results;
+    }
+
+    public Task<Stream> GetStreamAsync(IVideo video)
+    {
+        throw new NotSupportedException("Stream is already included with the video metadata.");
+    }
+}
